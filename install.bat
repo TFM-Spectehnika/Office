ba4afbc6d793d31bf685b7e1377dc9aaeb87961c
Kurlis Andrey <94551812+Warikos@users.noreply.github.com>
2021-11-24 11:33:01 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,7 @@ cd %SystemDrive%\users
 dir /s completedoffice.txt 
 if not errorlevel 1 goto end
 echo Running installer
-\\srv-db1\Config.1C$\Office2019\Setup.exe /configure \\srv-db1\Config.1C$\Office2019\configuration.xml
+\\net_share\Setup.exe /configure \\net_share\configuration.xml
 echo Office Install Complete > %SystemDrive%\users\completedoffice.txt
 :end
 exit
